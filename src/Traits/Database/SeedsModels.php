4fecfe25e95c0c48e51a789a77f3b03dc3c657b0
Javari <javari@duck.com>
2026-06-09 21:55:31 +0330

    feat: add model seeding utilities

---
+++ src/Traits/Database/SeedsModels.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Telefunction\Support\Traits\Database;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Seeder;
+
+/**
+ * @mixin Seeder
+ */
+trait SeedsModels
+{
+    /**
+     * @return array<class-string<Model>, array<int, array<string, mixed>>>
+     */
+    protected static function seeds(): array
+    {
+        return [];
+    }
+
+    protected static function forceSeed(): bool
+    {
+        return false;
+    }
+
+    public function run(): void
+    {
+        foreach (static::seeds() as $model => $records) {
+            $this->seedModel($model, $records);
+        }
+    }
+
+    /**
+     * @param class-string<Model> $model
+     * @param array<int, array<string, mixed>> $records
+     */
+    protected function seedModel(string $model, array $records): void
+    {
+        foreach ($records as $attributes) {
+            static::forceSeed()
+                ? $model::query()->forceCreate($attributes)
+                : $model::query()->create($attributes);
+        }
+    }
+}
